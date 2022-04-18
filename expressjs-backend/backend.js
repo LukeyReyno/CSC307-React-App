a6ef50bcbcd2c77ae2ac75d1fc043acc732546ad
Lucas Reyna <lucas.reyna143@gmail.com>
2022-04-18 13:51:54 -0700

    removed redundancies in backend.js due to database services

--- expressjs-backend/backend.js
+++ expressjs-backend/backend.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const userServices = require('./models/user-services');
+
 const app = express();
 const port = 5000;
 
@@ -13,37 +15,6 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
 });
-
-const users = { 
-    users_list :
-    [
-       { 
-          id : 'xyz789',
-          name : 'Charlie',
-          job: 'Janitor',
-       },
-       {
-          id : 'abc123', 
-          name: 'Mac',
-          job: 'Bouncer',
-       },
-       {
-          id : 'ppp222', 
-          name: 'Mac',
-          job: 'Professor',
-       }, 
-       {
-          id: 'yat999', 
-          name: 'Dee',
-          job: 'Aspring actress',
-       },
-       {
-          id: 'zap555', 
-          name: 'Dennis',
-          job: 'Bartender',
-       }
-    ]
-}
 
 app.get('/users', (req, res) => {
     const name = req.query.name;
@@ -68,22 +39,6 @@ app.get('/users', (req, res) => {
     }
 });
 
-/*const findUserByQuery = (queryVal, queryType) => { 
-    return users['users_list'].filter((user) => user[queryType] === queryVal);
-}*/
-
-const findUserByNameAndJob = (job, name) => { 
-    return users['users_list'].filter( (user) => user['job'] === job && user['name'] === name); 
-}
-
-const findUserByName = (name) => { 
-    return users['users_list'].filter( (user) => user['name'] === name); 
-}
-
-const findUserByJob = (job) => { 
-    return users['users_list'].filter( (user) => user['job'] === job); 
-}
-
 app.get('/users/:id', (req, res) => {
     const id = req.params['id']; //or req.params.id
     let result = findUserById(id);
@@ -95,26 +50,12 @@ app.get('/users/:id', (req, res) => {
     }
 });
 
-function findUserById(id) {
-    return users['users_list'].find( (user) => user['id'] === id); // or line below
-    //return users['users_list'].filter( (user) => user['id'] === id);
-}
-
 app.post('/users', (req, res) => {
     const userToAdd = req.body;
     userToAdd.id = idGen().toString();
     addUser(userToAdd);
     res.status(201).send(userToAdd);
 });
-
-function idGen() {
-    randomNum = Math.floor(Math.random() * 10000);
-    return randomNum;
-}
-
-function addUser(user) {
-    users['users_list'].push(user);
-}
 
 app.delete('/users/:id', (req, res) => {
     const id = req.params.id;
